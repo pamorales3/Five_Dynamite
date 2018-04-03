fdc37d0c93a02e151a51883a5a487dbc410f46d2
ajgarcia09 <anitajgr@gmail.com>
2018-04-03 11:21:24 -0600

    Added all tabs

--- output_views.py
+++ output_views.py
@@ -6,9 +6,27 @@ class OutputView(tk.Frame):
         tk.Frame.__init__(self, master)
         notebook = ttk.Notebook(self.master)
         notebook.pack()
-        dissected_stream_frame = ttk.Frame(notebook)
-        notebook.add(dissected_stream_frame, text="Dissected Stream Area")
-        tk.Button(dissected_stream_frame, text="click me").pack()
+        #Tab for Packet Stream Area
+        packet_stream_tab = ttk.Frame(notebook)
+        notebook.add(packet_stream_tab, text="Packet Stream Area")
+
+        #Tab for Dissected Stream Area
+        dissected_stream_tab = ttk.Frame(notebook)
+        notebook.add(dissected_stream_tab, text="Dissected Stream Area")
+        #Test button inside the tab
+        tk.Button(dissected_stream_tab, text="click me").pack()
+
+        #Tab for Raw Data Area
+        raw_data_tab = ttk.Frame(notebook)
+        notebook.add(raw_data_tab, text="Raw Data Area")
+
+        #Tab for Console Area
+
+        console_area_tab = ttk.Frame(notebook)
+        notebook.add(console_area_tab,text="Console Area")
+
+
+
 
 
 if __name__ == '__main__':
